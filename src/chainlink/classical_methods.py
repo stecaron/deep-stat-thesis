e74aea1b63e60427d92a64485639186ee4e37c07
Stephane Caron <ste.caron@icloud.com>
2019-10-09 10:15:26 -0400

    Try some classical clustering methods on chainlink dataset

---
+++ src/chainlink/classical_methods.py
@@ -0,0 +1,24 @@
+import numpy
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import SpectralClustering
+
+from src.chainlink.import_data import dt
+from src.chainlink.import_data import plot_chainlink
+
+X = numpy.array(dt[["x", "y", "z"]])
+
+# Real data
+plot_chainlink(dt[["x", "y", "z"]], dt["class"], show=True)
+
+# K-Means method
+kmeans = KMeans(n_clusters = 2, random_state=0).fit(X)
+plot_chainlink(dt[["x", "y", "z"]], kmeans.labels_, show=True)
+
+# Hierarchical Ward
+hclust_ward = AgglomerativeClustering(n_clusters=2, linkage="ward").fit(X)
+plot_chainlink(dt[["x", "y", "z"]], hclust_ward.labels_, show=True)
+
+# Spectral clustering
+spectral = SpectralClustering(n_clusters=2, affinity="precomputed").fit(X)
+plot_chainlink(dt[["x", "y", "z"]], spectral.labels_, show=True)
--- src/chainlink/import_data.py
+++ src/chainlink/import_data.py
@@ -7,16 +7,22 @@ from matplotlib import pyplot as plt
 dt = pandas.read_csv('src/chainlink/chainlink.csv')
 
 
-# Plot data
-groups = dt["class"].unique().tolist()
-colors = ("red", "blue")
-group_names = ("group1", "group2")
+def plot_chainlink(dt, labels, show=False):
 
-fig = plt.figure()  
-ax = fig.add_subplot(111, projection='3d')
-for group, color, group_name in zip(groups, colors, group_names):
-    dt_temp = dt.loc[dt["class"] == group]
-    ax.scatter(dt_temp["x"], dt_temp["y"], dt_temp["z"], alpha=0.8, c=color, label=group_name)
+    dt["group"] = labels
+    groups = dt["group"].unique().tolist()
+    colors = ("red", "blue")
+    group_names = ("group1", "group2")
 
-ax.legend()
-# plt.show()
+    fig = plt.figure()  
+    ax = fig.add_subplot(111, projection='3d')
+    for group, color, group_name in zip(groups, colors, group_names):
+        dt_temp = dt.loc[dt["group"] == group]
+        ax.scatter(dt_temp["x"], dt_temp["y"], dt_temp["z"], alpha=0.8, c=color, label=group_name)
+
+    ax.legend()
+
+    if show:
+        plt.show()
+
+    return(fig)
