66dbc89a616836f459a5a4cdcbf2e9505f3771b6
Stephane Caron <ste.caron@icloud.com>
2019-10-16 15:59:50 -0400

    Minor typos to experimental report

--- src/chainlink/classical_methods.py
+++ src/chainlink/classical_methods.py
@@ -9,16 +9,19 @@ from src.chainlink.import_data import plot_chainlink
 X = numpy.array(dt[["x", "y", "z"]])
 
 # Real data
-plot_chainlink(dt[["x", "y", "z"]], dt["class"], show=True, animate=True, name='real')
+plot_chainlink(dt[["x", "y", "z"]], dt["class"], show=True, animate=False, name='real')
 
 # K-Means method
 kmeans = KMeans(n_clusters = 2, random_state=0).fit(X)
-plot_chainlink(dt[["x", "y", "z"]], kmeans.labels_, show=True, animate=True, name='kmeans')
+plot_chainlink(dt[["x", "y", "z"]], kmeans.labels_, show=True, animate=False, name='kmeans')
 
 # Hierarchical Ward
 hclust_ward = AgglomerativeClustering(n_clusters=2, linkage="ward").fit(X)
-plot_chainlink(dt[["x", "y", "z"]], hclust_ward.labels_, show=True, animate=True, name='hclust')
+plot_chainlink(dt[["x", "y", "z"]], hclust_ward.labels_, show=True, animate=False, name='hclust')
+
+hclust_single = AgglomerativeClustering(n_clusters=2, linkage="single").fit(X)
+plot_chainlink(dt[["x", "y", "z"]], hclust_single.labels_, show=True, animate=False, name='hclust')
 
 # Spectral clustering
 spectral = SpectralClustering(n_clusters=2).fit(X)
-plot_chainlink(dt[["x", "y", "z"]], spectral.labels_, show=True, animate=True, name='spectral')
+plot_chainlink(dt[["x", "y", "z"]], spectral.labels_, show=True, animate=False, name='spectral')
