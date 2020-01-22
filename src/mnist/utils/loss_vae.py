9655b0b115fe2842b5a836cfbdf2161b7592b69c
Stephane Caron <ste.caron@icloud.com>
2020-01-22 09:07:59 -0500

    Add a print for the loss component percentage

--- src/mnist/utils/loss_vae.py
+++ src/mnist/utils/loss_vae.py
@@ -5,8 +5,8 @@ import torch.nn.functional as F
 def calculate_loss(x, reconstructed_x, mean, log_var, beta, loss_type="binary"):
     # reconstruction loss
     if loss_type == "binary":
-        #RCL = F.binary_cross_entropy(reconstructed_x, x, reduction="mean")
-        RCL = F.binary_cross_entropy(reconstructed_x, x, size_average=False)
+        RCL = F.binary_cross_entropy(reconstructed_x, x, reduction="sum")
+        #RCL = F.binary_cross_entropy(reconstructed_x, x, size_average=False)
     elif loss_type == "mse":
         RCL = F.mse_loss(reconstructed_x, x, reduction="mean")
 
@@ -16,5 +16,7 @@ def calculate_loss(x, reconstructed_x, mean, log_var, beta, loss_type="binary"):
     # Version mean
     #KLD = torch.mean(0.5 * torch.sum(torch.exp(log_var) + mean.pow(2) - 1 - log_var, 1))
     KLD = beta * KLD
+
+    print(KLD/(KLD + RCL))
 
     return RCL + KLD
