85ce19db8aea2f5abf67c087defdb6fb48030f27
Stephane Caron <ste.caron@icloud.com>
2020-01-22 13:43:17 -0500

    Remove shitty print

--- src/mnist/utils/loss_vae.py
+++ src/mnist/utils/loss_vae.py
@@ -17,6 +17,4 @@ def calculate_loss(x, reconstructed_x, mean, log_var, beta, loss_type="binary"):
     #KLD = torch.mean(0.5 * torch.sum(torch.exp(log_var) + mean.pow(2) - 1 - log_var, 1))
     KLD = beta * KLD
 
-    print(KLD/(KLD + RCL))
-
     return RCL + KLD, KLD
