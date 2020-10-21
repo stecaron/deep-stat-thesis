2bc50c4d39cde8d95a765c2b3746ae8b2f3762d4
Stephane Caron <stephane.caron@intact.net>
2020-10-21 11:29:36 -0400

    Add latest results

--- results/launch_ae.sh
+++ results/launch_ae.sh
@@ -1,20 +1,38 @@
 #! /bin/bash
 
-DATE="2020-08-05"
-N_TRAININGS=10
+DATE="2020-10-09"
+N_TRAININGS=20
 
-mkdir -p results/$DATE
+mkdir -p results/$DATE/ae
 
 for i in $(seq 1 $N_TRAININGS)
 do
-   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 5 --file "results/$DATE/ae_scenario1.csv"
-   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 5 9 --file "results/$DATE/ae_scenario2.csv"
-   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 0 2 3 4 5 6 7 8 9 --file "results/$DATE/ae_scenario3.csv"
+   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 5 --folder "results/$DATE/ae/" --file "scenario1_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 5 --folder "results/$DATE/ae/" --file "scenario1_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 5 --folder "results/$DATE/ae/" --file "scenario1_egal" --p_train 0.05 --p_test 0.05
+   
+   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 5 9 --folder "results/$DATE/ae/" --file "scenario2_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 5 9 --folder "results/$DATE/ae/" --file "scenario2_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 5 9 --folder "results/$DATE/ae/" --file "scenario2_egal" --p_train 0.05 --p_test 0.5
+   
+   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 0 2 3 4 5 6 7 8 9 --folder "results/$DATE/ae/" --file "scenario3_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 0 2 3 4 5 6 7 8 9 --folder "results/$DATE/ae/" --file "scenario3_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_ae --normal-digit 1 --anomalies 0 2 3 4 5 6 7 8 9 --folder "results/$DATE/ae/" --file "scenario3_egal" --p_train 0.05 --p_test 0.05
 
-   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 8 --file "results/$DATE/ae_scenario4.csv"
-   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 3 8 --file "results/$DATE/ae_scenario5.csv"
-   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 0 1 2 3 4 5 7 8 9 --file "results/$DATE/ae_scenario6.csv"
+   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 8 --folder "results/$DATE/ae/" --file "scenario4_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 8 --folder "results/$DATE/ae/" --file "scenario4_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 8 --folder "results/$DATE/ae/" --file "scenario4_egal" --p_train 0.05 --p_test 0.05
+   
+   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 3 8 --folder "results/$DATE/ae/" --file "scenario5_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 3 8 --folder "results/$DATE/ae/" --file "scenario5_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 3 8 --folder "results/$DATE/ae/" --file "scenario5_egal" --p_train 0.05 --p_test 0.05
+   
+   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 0 1 2 3 4 5 7 8 9 --folder "results/$DATE/ae/" --file "scenario6_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 0 1 2 3 4 5 7 8 9 --folder "results/$DATE/ae/" --file "scenario6_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_ae --normal-digit 6 --anomalies 0 1 2 3 4 5 7 8 9 --folder "results/$DATE/ae/" --file "scenario6_egal" --p_train 0.05 --p_test 0.05
 
-   python3 -m src.cars.main_ae --file "results/$DATE/ae_cars.csv"
+   python3 -m src.cars.main_ae --folder "results/$DATE/ae/" --file "scenario_cars_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.cars.main_ae --folder "results/$DATE/ae/" --file "scenario_cars_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.cars.main_ae --folder "results/$DATE/ae/" --file "scenario_cars_egal" --p_train 0.05 --p_test 0.05
 done
 
--- results/launch_vae_svm.sh
+++ results/launch_vae_svm.sh
@@ -1,20 +1,38 @@
 #! /bin/bash
 
-DATE="2020-08-05"
-N_TRAININGS=10
+DATE="2020-09-28"
+N_TRAININGS=20
 
-mkdir -p results/$DATE
+mkdir -p results/$DATE/vae_svm
 
 for i in $(seq 1 $N_TRAININGS)
 do
-    python3 -m src.cars.main_vae_svm --file "results/$DATE/vae_svm_cars.csv"
-    #python3 -m src.mnist.main_vae_svm --normal-digit 1 --anomalies 5 --file "results/$DATE/vae_svm_scenario1.csv"
-    #python3 -m src.mnist.main_vae_svm --normal-digit 1 --anomalies 5 9 --file "results/$DATE/vae_svm_scenario2.csv"
-    #python3 -m src.mnist.main_vae_svm --normal-digit 1 --anomalies 0 2 3 4 5 6 7 8 9 --file "results/$DATE/vae_svm_scenario3.csv"
+   python3 -m src.mnist.main_svm --normal-digit 1 --anomalies 5 --folder "results/$DATE/vae_svm/" --file "scenario1_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_svm --normal-digit 1 --anomalies 5 --folder "results/$DATE/vae_svm/" --file "scenario1_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_svm --normal-digit 1 --anomalies 5 --folder "results/$DATE/vae_svm/" --file "scenario1_egal" --p_train 0.05 --p_test 0.05
+   
+   python3 -m src.mnist.main_svm --normal-digit 1 --anomalies 5 9 --folder "results/$DATE/vae_svm/" --file "scenario2_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_svm --normal-digit 1 --anomalies 5 9 --folder "results/$DATE/vae_svm/" --file "scenario2_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_svm --normal-digit 1 --anomalies 5 9 --folder "results/$DATE/vae_svm/" --file "scenario2_egal" --p_train 0.05 --p_test 0.5
+   
+   python3 -m src.mnist.main_svm --normal-digit 1 --anomalies 0 2 3 4 5 6 7 8 9 --folder "results/$DATE/vae_svm/" --file "scenario3_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_svm --normal-digit 1 --anomalies 0 2 3 4 5 6 7 8 9 --folder "results/$DATE/vae_svm/" --file "scenario3_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_svm --normal-digit 1 --anomalies 0 2 3 4 5 6 7 8 9 --folder "results/$DATE/vae_svm/" --file "scenario3_egal" --p_train 0.05 --p_test 0.05
 
-    #python3 -m src.mnist.main_vae_svm --normal-digit 6 --anomalies 8 --file "results/$DATE/vae_svm_scenario4.csv"
-    #python3 -m src.mnist.main_vae_svm --normal-digit 6 --anomalies 3 8 --file "results/$DATE/vae_svm_scenario5.csv"
-    #python3 -m src.mnist.main_vae_svm --normal-digit 6 --anomalies 0 1 2 3 4 5 7 8 9 --file "results/$DATE/vae_svm_scenario6.csv"
+   python3 -m src.mnist.main_svm --normal-digit 6 --anomalies 8 --folder "results/$DATE/vae_svm/" --file "scenario4_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_svm --normal-digit 6 --anomalies 8 --folder "results/$DATE/vae_svm/" --file "scenario4_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_svm --normal-digit 6 --anomalies 8 --folder "results/$DATE/vae_svm/" --file "scenario4_egal" --p_train 0.05 --p_test 0.05
+   
+   python3 -m src.mnist.main_svm --normal-digit 6 --anomalies 3 8 --folder "results/$DATE/vae_svm/" --file "scenario5_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_svm --normal-digit 6 --anomalies 3 8 --folder "results/$DATE/vae_svm/" --file "scenario5_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_svm --normal-digit 6 --anomalies 3 8 --folder "results/$DATE/vae_svm/" --file "scenario5_egal" --p_train 0.05 --p_test 0.05
+   
+   python3 -m src.mnist.main_svm --normal-digit 6 --anomalies 0 1 2 3 4 5 7 8 9 --folder "results/$DATE/vae_svm/" --file "scenario6_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.mnist.main_svm --normal-digit 6 --anomalies 0 1 2 3 4 5 7 8 9 --folder "results/$DATE/vae_svm/" --file "scenario6_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.mnist.main_svm --normal-digit 6 --anomalies 0 1 2 3 4 5 7 8 9 --folder "results/$DATE/vae_svm/" --file "scenario6_egal" --p_train 0.05 --p_test 0.05
 
+   python3 -m src.cars.main_svm --folder "results/$DATE/vae_svm/" --file "scenario_cars_plus" --p_train 0.01 --p_test 0.1
+   python3 -m src.cars.main_svm --folder "results/$DATE/vae_svm/" --file "scenario_cars_moins" --p_train 0.1 --p_test 0.01
+   python3 -m src.cars.main_svm --folder "results/$DATE/vae_svm/" --file "scenario_cars_egal" --p_train 0.05 --p_test 0.05
 done
 
